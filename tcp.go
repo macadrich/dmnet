c95a99f54f1e5534eb8549e7c2f020e3dc2c548c
macadrich <artiza.adriel@gmail.com>
2020-02-21 10:24:43 +0800

    add source

---
+++ tcp.go
@@ -0,0 +1,6 @@
+package dmnet
+
+// Add addition of two numbers
+func Add(a, b int) int {
+	return a + b
+}
---
+++ tcp_test.go
@@ -0,0 +1,13 @@
+package dmnet
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	const n = 5
+	const m = 4
+	if ans := Add(n, m); ans != 9 {
+		t.Errorf("Error answer: %v", ans)
+	}
+}
